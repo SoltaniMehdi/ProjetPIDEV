5909c3136b86631bfc7eb979afa6e7f7012c425c
ela <ela.marzouki@esprit.tn>
2024-04-28 21:17:41 +0100

    3rd time success

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -7,8 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Stripe;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Twilio\Rest\Client; 
 
 
 class StripeController extends AbstractController
@@ -21,30 +20,37 @@ class StripeController extends AbstractController
         ]);
     }
 
-    #[Route('/stripe/create-charge', name: 'app_stripe_charge', methods: ['POST'])]
-    public function createCharge(Request $request, MailerInterface $mailer): Response
-    {
-        Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
-        Stripe\Charge::create([
-            "amount" => 5 * 100, // Adjust the amount as needed
-            "currency" => "usd", // Adjust the currency as needed
-            "source" => $request->request->get('stripeToken'),
-            "description" => "Binaryboxtuts Payment Test"
-        ]);
 
-        // Send confirmation email
-        $email = (new Email())
-            ->from('elamarzouky00@gmail.com') // Adjust sender email address
-            ->to('elamarzouky00@gmail.com') // Adjust recipient email address
-            ->subject('Payment Confirmation')
-            ->text('Your payment was successful.'); // Email body
 
-        $mailer->send($email);
+#[Route('/stripe/create-charge', name: 'app_stripe_charge', methods: ['POST'])]
+public function createCharge(Request $request): Response
+{
+    Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+    Stripe\Charge::create([
+        "amount" => 5 * 100, // Adjust the amount as needed
+        "currency" => "usd", // Adjust the currency as needed
+        "source" => $request->request->get('stripeToken'),
+        "description" => "Binaryboxtuts Payment Test"
+    ]);
 
-        $this->addFlash(
-            'success',
-            'Payment Successful!'
-        );
-        return $this->redirectToRoute('app_stripe', [], Response::HTTP_SEE_OTHER);
-    }
+    // Send confirmation SMS
+    $sid = $_ENV["TWILIO_SID"];
+    $token = $_ENV["TWILIO_AUTH_TOKEN"];
+    $twilio = new Client($sid, $token);
+    $twilio->messages
+           ->create(
+               '+21625175698', // Adjust recipient phone number
+               [
+                   'from' => '+17577932884',
+                   'body' => 'Bravo ! Votre achat est confirmé. Votre paiement a été accepté avec succès. Bienvenue sur VitaPlat, votre destination privilégiée pour des plats sains de qualité. Explorez nos offres exclusives dès maintenant ! Merci pour votre confiance.'
+               ]
+           );
+
+    $this->addFlash(
+        'success',
+        'Payment Successful!'
+    );
+    return $this->redirectToRoute('app_stripe', [], Response::HTTP_SEE_OTHER);
+}
+
 }
